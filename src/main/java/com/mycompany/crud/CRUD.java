3eed379039ba0a8f7fbfa055a7d18b53584a189f
LuisQ <ingluisqm@gmail.com>
2023-04-29 13:35:01 -0500

    Texto

--- src/main/java/com/mycompany/crud/CRUD.java
+++ src/main/java/com/mycompany/crud/CRUD.java
@@ -12,5 +12,7 @@ public class CRUD {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
